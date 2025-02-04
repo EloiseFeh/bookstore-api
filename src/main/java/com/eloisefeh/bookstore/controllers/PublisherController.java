933cd79a89adf6044e376277016db840292c90b2
Eloise Ferreira <eloiseferreira045@gmail.com>
2025-02-04 10:52:39 -0300

    feature: Crud publisher

--- src/main/java/com/eloisefeh/bookstore/controllers/PublisherController.java
+++ src/main/java/com/eloisefeh/bookstore/controllers/PublisherController.java
@@ -4,12 +4,14 @@ import com.eloisefeh.bookstore.dtos.PublisherRecordDto;
 import com.eloisefeh.bookstore.models.PublisherModel;
 import com.eloisefeh.bookstore.services.PublisherService;
 import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/publisher")
@@ -19,15 +21,54 @@ public class PublisherController {
     private PublisherService publisherService;
 
     @PostMapping
-    public ResponseEntity<PublisherModel> createPublisher(@RequestBody @Valid PublisherRecordDto publisher){
-        PublisherModel createdPublisher = publisherService.savePublisher(publisher);
-        return new ResponseEntity<>(createdPublisher, HttpStatus.CREATED);
+    public ResponseEntity<Object> createPublisher(@RequestBody @Valid PublisherRecordDto publisherRecordDto){
+        var publisherModel = new PublisherModel();
+        BeanUtils.copyProperties(publisherRecordDto, publisherModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(publisherService.savePublisher(publisherModel));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updatePublisher(@PathVariable Long id,
+                                                  @RequestBody @Valid PublisherRecordDto publisherDto){
+        Optional<PublisherModel> publisherOptional = publisherService.findById(id);
+        if (!publisherOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Editora não encontrada");
+        }
+
+        var publisherModel = new PublisherModel();
+        BeanUtils.copyProperties(publisherDto, publisherModel);
+        publisherModel.setId(publisherOptional.get().getId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(publisherService.savePublisher(publisherModel));
     }
 
     @GetMapping
     public ResponseEntity<List<PublisherModel>> getAllPublisher(){
         List<PublisherModel> publishers = publisherService.findAll();
         return ResponseEntity.ok(publishers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getPublisherById(@PathVariable Long id){
+        Optional<PublisherModel> publisher = publisherService.findById(id);
+        if (!publisher.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Editora não encontrada");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(publisher.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletePublisher(@PathVariable Long id){
+        Optional<PublisherModel> publisher = publisherService.findById(id);
+
+        if (!publisher.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Editora não encontrada");
+        }
+        publisherService.delete(publisher.get());
+
+        return ResponseEntity.status(HttpStatus.OK).body("Editora deletada com sucesso!");
     }
 
 }
--- src/main/java/com/eloisefeh/bookstore/services/PublisherService.java
+++ src/main/java/com/eloisefeh/bookstore/services/PublisherService.java
@@ -6,8 +6,10 @@ import com.eloisefeh.bookstore.repositories.PublisherRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PublisherService {
@@ -15,17 +17,20 @@ public class PublisherService {
     @Autowired
     private PublisherRepository publisherRepository;
 
-    public PublisherModel savePublisher(PublisherRecordDto publisherDto){
-        PublisherModel publisherModel = new PublisherModel();
-        publisherModel.setName(publisherDto.name());
-        publisherModel.setEmail(publisherDto.email());
-        publisherModel.setPhone(publisherDto.phone());
-        publisherModel.setSite(publisherDto.site());
-
+    public PublisherModel savePublisher(PublisherModel publisherModel){
         return publisherRepository.save(publisherModel);
     }
 
     public List<PublisherModel> findAll() {
         return publisherRepository.findAll();
+    }
+
+    public Optional<PublisherModel> findById(Long id){
+        return publisherRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(PublisherModel publisherModel){
+        publisherRepository.delete(publisherModel);
     }
 }
